b72cd97f2635acdb103ac4a85ee28b68088d765c
Nadeem-2005 <mr.maestro002@gmail.com>
2026-02-09 15:56:21 +0530

    Added profiles

--- src/test/java/com/nadeem/journalApp/JournalAppApplicationTests.java
+++ src/test/java/com/nadeem/journalApp/JournalAppApplicationTests.java
@@ -1,13 +1,48 @@
 package com.nadeem.journalApp;
 
+import com.nadeem.journalApp.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authorization.method.AuthorizeReturnObject;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest //this annotation is to say to spring to start an application context that is similar to normal exec of app
 class JournalAppApplicationTests {
 
+	//test name could be anything
+	//tests are annotated by @Test
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	public void testFindByUsername() {
+		assertNotNull(userRepository.findByUserName("nadeem")); //passes
+//		assertNotNull(userRepository.findByUserName("nad")); //fails
+	}
+
+	/**
+	 *
+	 * @param a
+	 * @param b
+	 * @param expected
+	 *
+	 * this annotation is used to run a test with multiple values.
+	 * these values can be passed with @CSV or @CSVSource
+	 */
+
+	@ParameterizedTest // to show that this test is parameterized
+	@CsvSource({
+			"1,1,2",
+			"2,2,4",
+			"3,5,8"
+	})
+	public void test(int a, int b, int expected){
+		assertEquals(expected, a+b);
 	}
 
 }
